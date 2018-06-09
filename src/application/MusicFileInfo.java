f8d4b56ac9a4f10663bbe9df6d42102512c3dc91
LAPTOP-3KRNTOCR\patry <patrycja@karbownik.net>
2018-06-09 20:34:38 +0200

    niewiele wiecej z informacji o utworze

--- src/application/MusicFileInfo.java
+++ src/application/MusicFileInfo.java
@@ -1,5 +1,6 @@
 package application;
 
+import main.com.mpatric.mp3agic.ID3v2;
 import main.com.mpatric.mp3agic.Mp3File;
 
 public class MusicFileInfo {
@@ -35,7 +36,15 @@ public class MusicFileInfo {
 
         try {
             mp3File = new Mp3File(ChooseFile.getFilePath());
+            if(mp3File.hasId3v2Tag()) {
+                ID3v2 tag = mp3File.getId3v2Tag();
+                String title = tag.getTitle();
+                String album = tag.getAlbum();
+                String artist = tag.getArtist();
+               // if()
 
+                
+            }
 
 
         } catch (Exception e) {
